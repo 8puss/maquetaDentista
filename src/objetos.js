477ad05bd56e321905e46cf5a97b175cec00563b
aleph_0 <xoor@riseup.net>
2021-02-10 00:06:22 -0600

    first commit

---
+++ src/objetos.js
@@ -0,0 +1,17 @@
+class Cliente {
+    constructor(nombre, contraseña, edad, correo) {
+        this.nombre = nombre 
+        this.contraseña = contraseña;
+        this.correo = correo;
+        this.edad = edad;
+    }
+}
+
+class Dentista {
+    constructor(nombre, contraseña, edad,correo) {
+        this.nombre = nombre 
+        this.contraseña = contraseña;
+        this.correo = correo;
+        this.edad = edad;
+    }
+}
