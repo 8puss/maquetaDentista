860cbfe3a576269e3ededf17ffef3a0bc387d9c9
aleph_0 <xoor@riseup.net>
2021-02-15 17:04:30 -0600

    express code 4 server

---
+++ src/servidor.js
@@ -0,0 +1,7 @@
+/* Declaración de variables y constantes para establecer un servidor local con Express */
+
+const express = require("express");
+const app = express();
+var path = require('path');
+var http = require("http");
+var PORT = 5000;
