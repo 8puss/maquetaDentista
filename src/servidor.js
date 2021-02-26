43e40189018ad6ea0a6b79311d660ecee0b982f6
aleph_0 <xoor@riseup.net>
2021-02-25 21:52:20 -0600

    update

--- src/servidor.js
+++ src/servidor.js
@@ -5,7 +5,14 @@ const app = express();
 var path = require('path');
 var http = require("http");
 var PORT = 5000;
+var opciones = {
+    root: path.join(__dirname)
+};
 
 app.get('/', function(req, res, next) {
+    res.sendFile('/../public/template/index.html', opciones);
+});
 
+const servidor = app.listen(PORT, function(){
+    console.log('Listeing http://localHost:' + PORT);
 });
